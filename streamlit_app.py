18b28ea4789fb2282a315a7f6281013c818a9360
REGU RAM S V <63114063+svreguram@users.noreply.github.com>
2022-11-29 12:46:46 +0530

    Update streamlit_app.py
    adding header and text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
-streamlit.title('My Snowflake Badge 2 course')
+streamlit.title('My Profile details')
+
+streamlit.header('Cloud Tools')
+streamlit.text('Snowflake')
+streamlit.text('Informatica')
+streamlit.text('PowerBI')
+streamlit.text('Databricks')
