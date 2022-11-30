8ecd35fea98c4a6d47e1e9dcc230c528e19b32df
REGU RAM S V <63114063+svreguram@users.noreply.github.com>
2022-11-30 08:23:17 +0530

    Update streamlit_app.py
    adding emoji and additional header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,11 @@
 import streamlit
 
-streamlit.title('My Profile details')
+streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Cloud Tools')
-streamlit.text('Snowflake')
-streamlit.text('Informatica')
-streamlit.text('PowerBI')
-streamlit.text('Databricks')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
